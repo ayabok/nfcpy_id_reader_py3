533d5de39d0694ab6ae03101732cd51bbe82bfca
ayabok <126740956+ayabok@users.noreply.github.com>
2024-10-19 20:46:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(
     name='nfcpy_id_reader',
     version='0.1.0',
     description='Read the ID of an NFC Tag with nfcpy',
-    url='https://github.com/mascii/nfcpy_id_reader',
-    author='Masaki Koyanagi',
-    author_email='mascii@gmail.com',
+    url='https://github.com/ayabok/nfcpy_id_reader_py3',
+    author='ayabok',
+    author_email='ayabok@outlook.com',
     license='EUPL 1.1',
     keywords='nfc pasori',
     packages=['nfcpy_id_reader'],
     install_requires=requires,
     classifiers=[
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
 )
 
